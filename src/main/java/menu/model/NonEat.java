8be764590d70b967b58168f2e2c7413f2ae6787d
DongYeop <s01054830455@gmail.com>
2024-12-11 19:46:53 +0900

    feat: 못 먹는 메뉴에 대한 예외처리

--- src/main/java/menu/model/NonEat.java
+++ src/main/java/menu/model/NonEat.java
@@ -1,5 +1,6 @@
 package menu.model;
 
+import java.util.HashSet;
 import java.util.List;
 import menu.exception.ErrorMessage;
 
@@ -8,13 +9,28 @@ public class NonEat {
     private final List<String> nonEat;
 
     public NonEat(List<String> nonEat) {
+        validate(nonEat);
+        validateDuplicate(nonEat);
         validateSize(nonEat);
         this.nonEat = nonEat;
+    }
+
+    private void validate(List<String> nonEat) {
+        if (!nonEat.contains("")) {
+            Menu.validateContain(nonEat);
+        }
     }
 
     private void validateSize(List<String> nonEat) {
         if (nonEat.size() > 2) {
             throw new IllegalArgumentException(ErrorMessage.NON_EAT_SIZE.getErrorMessage());
+        }
+    }
+
+    private void validateDuplicate(List<String> nonEat) {
+        HashSet<String> nonDuplicateNonEatSet = new HashSet<>(nonEat);
+        if (nonDuplicateNonEatSet.size() != nonEat.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_MENU.getErrorMessage());
         }
     }
 }
